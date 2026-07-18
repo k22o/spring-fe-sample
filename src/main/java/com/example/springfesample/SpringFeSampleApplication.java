b85b4c82feacc4e04340fd4fbcb51e4ad3ae8760
k22o <kana.t3.105@gmail.com>
2026-07-18 22:05:57 +0900

    :toda: first commit

---
+++ src/main/java/com/example/springfesample/SpringFeSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfesample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFeSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFeSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springfesample/SpringFeSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfesample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFeSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
